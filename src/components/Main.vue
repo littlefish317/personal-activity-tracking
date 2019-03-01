a107e12ee989b28c608cb9e927badc1df94b5a2f
Katie Uhl <katie.uhl@hotmail.com>
2019-02-28 21:35:38 -0500

    KU practice syntax

---
+++ src/components/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <h2>I am {{ name }}</h2>
+    <input v-model="age" />
+    <ul v-if="quests">
+      <li v-for="quest in quests" :key="quest.id">
+        {{ quest.id }} {{ quest.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Katie",
+      age: 15,
+      quests: [
+        {
+          id: 1,
+          name: "first"
+        },
+        {
+          id: 2,
+          name: "second"
+        }
+      ]
+    };
+  },
+  computed: {
+    shouldId: function() {
+      if (this.age < 21) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped></style>
